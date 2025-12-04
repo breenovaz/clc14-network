e877a763d3be61f1cd4f94742a4287805d1f9c45
Breno Vaz <breenovaaz@hotmail.com>
2025-12-04 23:58:21 +0000

    correcao

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "breno-terraform-CLC14-NOVO"
+    bucket = "breno-terraform-clc14-new"
     key    = "network/terraform.tfstate"
     region = "us-east-1"
      use_lockfile = true
--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "minha_vpc" {
 
 # Correcao primeira issue
 resource "aws_flow_log" "example" {
-  log_destination      = "arn:aws:s3:::breno-terraform-CLC14-NOVO"
+  log_destination      = "arn:aws:s3:::breno-terraform-clc14-new"
   log_destination_type = "s3"
   traffic_type         = "ALL"
   vpc_id               = aws_vpc.minha_vpc.id
