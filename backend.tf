ba69a1e5256d4df61fc7ba482de3938c712eb91c
Breno Vaz <breenovaaz@hotmail.com>
2025-12-04 23:50:36 +0000

    correcao

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "breno-terraform-clc14"
+    bucket = "breno-terraform-CLC14-NOVO"
     key    = "network/terraform.tfstate"
     region = "us-east-1"
      use_lockfile = true
